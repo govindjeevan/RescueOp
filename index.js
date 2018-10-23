695f41fe2676b6c710f710c684c01530f6add630
Govind Jeevan <govindjeevan7@gmail.com>
2018-10-23 21:55:10 +0530

    CORS

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(morgan('dev'));  
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  
 app.use('/api',routes);
 
 // erro handling middle-ware
@@ -32,7 +40,7 @@ app.use(function(err,req,res,next){
     res.status(422).send({error: err.message})
 })
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
